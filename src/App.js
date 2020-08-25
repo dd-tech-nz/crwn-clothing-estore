5f0ec767577bb97470211a044f7256ce9899ab72
dd-tech-nz <ddumolo@hotmail.com>
2020-08-25 23:52:24 +1200

    install styled components and convert Homepage, Header and CustomButton to use CSS in JS. Create addCollectionAndDocuments function to firebase.utils to facilitate uploading stored collections  data to Firebase

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Header from './components/header/header.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
-import {selectCurrentUser } from './redux/user/user.selectors'
+import { selectCurrentUser } from './redux/user/user.selectors'
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -68,7 +68,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector ({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -25,7 +25,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     const createdAt = new Date()
     try {
       await userRef.set({
-        displayName, 
+        displayName,
         email,
         createdAt,
         ...additionalData
@@ -35,6 +35,19 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     }
   }
 return userRef
+}
+
+export const addCollectionAndDocuments = async (
+  collectionKey, objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey)
+
+  const batch = firestore.batch()
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc()
+    batch.set(newDocRef, obj)
+  })
+  return await batch.commit()
 }
 
 firebase.initializeApp(config)
